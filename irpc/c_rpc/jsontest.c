913ba72fbe7b57da629f38cb7f26ff883cd1d4d5
Henry Doupe <hdoupe@yahoo.com>
2018-07-03 11:42:19 -0400

    Parse simple JSON file

--- irpc/c_rpc/jsontest.c
+++ irpc/c_rpc/jsontest.c
@@ -1,62 +1,57 @@
 #include <cjson/cJSON.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
 
+struct params {
+    char *mtr_wrt_group;
+    char *file_name;
+};
 
-/* return 1 if the monitor supports full hd, 0 otherwise */
-int supports_full_hd(const char * const monitor)
+struct params makeparams(const char * const params)
 {
-    const cJSON *resolution = NULL;
-    const cJSON *resolutions = NULL;
-    const cJSON *name = NULL;
-    int status = 0;
-    cJSON *monitor_json = cJSON_Parse(monitor);
-    if (monitor_json == NULL)
+    struct params input;
+    cJSON *json_obj = cJSON_Parse(params);
+    if (json_obj == NULL)
     {
         const char *error_ptr = cJSON_GetErrorPtr();
         if (error_ptr != NULL)
         {
             fprintf(stderr, "Error before: %s\n", error_ptr);
         }
-        status = 0;
-        goto end;
+        cJSON_Delete(json_obj);
+        return input;
     }
 
-    name = cJSON_GetObjectItemCaseSensitive(monitor_json, "name");
-    if (cJSON_IsString(name) && (name->valuestring != NULL))
-    {
-        printf("Checking monitor \"%s\"\n", name->valuestring);
+    cJSON *mtr_wrt_group = cJSON_GetObjectItemCaseSensitive(json_obj, "mtr_wrt_group");
+    cJSON *file_name = cJSON_GetObjectItemCaseSensitive(json_obj, "file_name");
+
+    // we are expecting strings
+    // TODO: figure out better return type
+    if (!cJSON_IsString(mtr_wrt_group) || !cJSON_IsString(file_name)) {
+        printf("\"mtr_wrt_group\" and \"file_name\" should be strings\n");
+        cJSON_Delete(json_obj);
+        return input;
     }
 
-    resolutions = cJSON_GetObjectItemCaseSensitive(monitor_json, "resolutions");
-    cJSON_ArrayForEach(resolution, resolutions)
-    {
-        cJSON *width = cJSON_GetObjectItemCaseSensitive(resolution, "width");
-        cJSON *height = cJSON_GetObjectItemCaseSensitive(resolution, "height");
+    // allocate memory to struct fields and copy data to them
+    input.mtr_wrt_group = malloc(strlen(mtr_wrt_group->valuestring) + 1);
+    input.file_name = malloc(strlen(file_name->valuestring) + 1);
+    strcpy(input.mtr_wrt_group, mtr_wrt_group->valuestring);
+    strcpy(input.file_name, file_name->valuestring);
 
-        printf("width: %d\n", width->valueint);
+    printf("received data: %s %s\n", input.mtr_wrt_group, input.file_name);
 
-        if (!cJSON_IsNumber(width) || !cJSON_IsNumber(height))
-        {
-            status = 0;
-            goto end;
-        }
-
-        if ((width->valuedouble == 1920) && (height->valuedouble == 1080))
-        {
-            status = 1;
-            goto end;
-        }
-    }
-
-end:
-    cJSON_Delete(monitor_json);
-    return status;
+    cJSON_Delete(json_obj);
+    return input;
 }
 
 int main (void){
-    char *teststring = "{\"name\": \"Awesome 4K\", \"resolutions\": [{\"width\": 1280, \"height\": 720}, {\"width\": 1920, \"height\": 1080}, {\"width\": 3840, \"height\": 2160}]}";
-    supports_full_hd (teststring);
+    // char *teststring = "{\"name\": \"Awesome 4K\", \"resolutions\": [{\"width\": 1280, \"height\": 720}, {\"width\": 1920, \"height\": 1080}, {\"width\": 3840, \"height\": 2160}]}";
+    char *teststring = "{\"mtr_wrt_group\": \"full\", \"file_name\": \"taxsimrun.txt\"}";
+    struct params input;
+    input = makeparams (teststring);
+    printf("received data: %s %s\n", input.mtr_wrt_group, input.file_name);
 }
