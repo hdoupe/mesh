772089ca238a32589f5574958a298c4aacbe3570
Henry Doupe <hdoupe@yahoo.com>
2018-07-08 11:22:30 -0400

    Clean up docker file

--- dev_notes/testfortran.for
+++ dev_notes/testfortran.for
@@ -1,7 +1,6 @@
 c to convert to f77, comment out open statements and call to date and time
 c and fix zeros in format statements 200 and 201 in out. Also change
 c format to * for f2c.
-
       subroutine runmodel() bind (C)
       use iso_c_binding, only: C_CHAR, c_null_char
       implicit none
