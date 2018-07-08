0d17cd669ff528f1750570e42d57b71e60ab8bf1
Henry Doupe <hdoupe@yahoo.com>
2018-07-08 12:12:03 -0400

    Pass argument to fortran too

--- dev_notes/taxsimlink.c
+++ dev_notes/taxsimlink.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-extern void runmodel(char [], int);
+extern void runmodel(char [], int, char [], int);
 
 int main(){
     char fname[] = "data.txt";
     int fnamesize = sizeof(fname)/sizeof(fname[0]);
-    runmodel(fname, fnamesize);
+    char arg[] = "full";
+    int argsize = sizeof(arg)/sizeof(fname[0]);
+    runmodel(fname, fnamesize, arg, argsize);
     return 0;
 }
--- dev_notes/testfortran.for
+++ dev_notes/testfortran.for
@@ -6,12 +6,13 @@ c format to * for f2c.
       implicit none
 C      character*64 fname
       contains
-      subroutine runmodel(fname, n) bind (C)
+      subroutine runmodel(fname, fnamesize, arg, argsize) bind (C)
       use iso_c_binding, only: C_CHAR, c_null_char
-      integer(c_int), value :: n
-      character(kind = c_char) :: fname(n)
-C      character*64, intent(in) :: fname
-      print*, 'hey there', fname
+      integer(c_int), value :: fnamesize
+      character(kind = c_char) :: fname(fnamesize)
+      integer(c_int), value :: argsize
+      character(kind = c_char) :: arg(argsize)
+      print*, 'hey there', fname, arg
       return
       end subroutine runmodel
       end module model
