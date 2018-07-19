409b558673a1d7e07ed577a6379057d346dfc031
Henry Doupe <hdoupe@yahoo.com>
2018-07-19 10:11:21 -0400

    Working prototype

--- dev_notes/taxsimlink.c
+++ dev_notes/taxsimlink.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-extern void runmodel(char [], int, char [], int, char [], int);
+extern void runmodel(char [], int, char [], int, char *, int);
 
 int main(){
     char fname[] = "data.txt";
     int fnamesize = sizeof(fname)/sizeof(fname[0]);
     char arg[] = "full";
     int argsize = sizeof(arg)/sizeof(arg[0]);
-    char buffer[] = "hello there friend";
-    int buffersize = sizeof(buffer)/sizeof(buffer[0]);
+    char *buffer;
+    int buffersize = 20;
+    buffer = (char*) malloc(sizeof(char) * buffersize);
     runmodel(fname, fnamesize, arg, argsize, buffer, buffersize);
-    printf("got result %s\n", buffer);
+    printf("got result: %s\n", buffer);
     return 0;
 }
