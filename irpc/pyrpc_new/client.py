f046a3428bb1e0107f6629a8a6fc1b8b60d06efa
Lucas Szwarcberg <lucas.szwarcberg@gmail.com>
2018-08-14 14:06:54 -0400

    Retrieve data from TaxCalc

--- irpc/pyrpc_new/client.py
+++ irpc/pyrpc_new/client.py
@@ -7,16 +7,12 @@ _local_netref_attrs = frozenset([
     '__getattribute__', '__methods__', '__init__', '__metaclass__',
     '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__',
     '__setattr__', '__slots__', '__str__', '__weakref__', '__dict__',
-    '__members__',  '__exit__', '__hash__', '__call__'
+    '__members__',  '__exit__', '__hash__', '__call__', '____deref__'
 ])
 """the set of attributes that are local to the netref object"""
 
 
-_immutable_builtin_types = [
-    bool, float, int, str, type(None)]
-"""a list of immutable types built into JSON/msgpack"""
-
-classes_cache = []
+netrefs = {}
 
 
 def syncreq(client, endpoint, *args, **kwargs):
@@ -26,6 +22,14 @@ def syncreq(client, endpoint, *args, **kwargs):
     r = client.get(j)
     print(j['result'])
     return j['result']
+
+
+def asyncreq(client, endpoint, *args, **kwargs):
+    # TODO: Make this asynchronous (don't wait for reply)
+    syncreq(client, endpoint, *args, **kwargs)
+
+def deref(netref):
+    return netref.____deref__()
 
 
 class BaseNetref(object):
@@ -56,8 +60,27 @@ class BaseNetref(object):
             syncreq(self, 'handle_call',
                     oid=self.____oid__, callargs=args, callkwargs=kwargs))
 
+    def __iter__(self):
+        return self.__iter__()
+
+    def __next__(self):
+        return self.__next__()
+
+    def __len__(self):
+        return self.__len__()
+
+    def ____deref__(self):
+        return unwrap_result(
+            self.____cli__,
+            syncreq(self, 'handle_deref', self.____oid__))
+
+    def __del__(self):
+        try:
+            asyncreq(self, 'handle_del', self.____oid__)
+        except Exception:
+            pass
+
     # To implement:
-    # - __del__
     # - __delattr__
     # - __setattr__
     # - __dir__
@@ -78,16 +101,11 @@ class GenericNetref(BaseNetref):
 def unwrap_result(client, r):
     if 'result' in r:
         return r['result']
+    elif r['oid'] in netrefs:
+        return netrefs[r['oid']]
     else:
         return GenericNetref(client, r['oid'], r['otype'])
 
 
 def get_remote(client, varname):
     return unwrap_result(client, syncreq(client, 'handle_get_remote', varname))
-
-
-if __name__ == '__main__':
-    with Client(serializer='msgpack') as cli:
-        a = get_remote(cli, 'Test')
-        b = a()
-        print("Got " + b.some_method())
--- irpc/pyrpc_new/kernel.py
+++ irpc/pyrpc_new/kernel.py
@@ -1,19 +1,27 @@
 from rpc.kernel import Kernel
-from client import _immutable_builtin_types
 from uuid import uuid1
+from rpyc.lib.colls import RefCountingColl
+
+
+_immutable_builtin_types = [
+    bool, float, int, str, type(None)]
+"""a list of immutable types built into JSON/msgpack"""
 
 
 class Test:
     def some_method(self):
         return "aa"
 
+    def another_method(self):
+        import taxcalc
+        return taxcalc.Records.cps_constructor().e00200.tolist()
+
 
 exported_vars = {
     "Test": Test
 }
 
-
-myvars = {}
+imported_vars = {}
 
 
 def wrap_result(r):
@@ -22,11 +30,19 @@ def wrap_result(r):
     if cls in _immutable_builtin_types:
         res['result'] = r
     else:
-        oid = str(uuid1())
-        myvars[oid] = r
+        oid = id(r)
+        imported_vars[oid] = r
         res['otype'] = cls.__name__
         res['oid'] = oid
     return res
+
+
+def handle_deref(oid):
+    return {'result': imported_vars[oid]}
+
+
+def handle_del(oid):
+    del imported_vars[oid]
 
 
 def handle_get_remote(varname):
@@ -34,15 +50,17 @@ def handle_get_remote(varname):
 
 
 def handle_getattr(oid, attrname):
-    return wrap_result(getattr(myvars[oid], attrname))
+    return wrap_result(getattr(imported_vars[oid], attrname))
 
 
 def handle_call(oid, callargs, callkwargs):
-    return wrap_result(myvars[oid](*callargs, **callkwargs))
+    return wrap_result(imported_vars[oid](*callargs, **callkwargs))
 
 
 kernel = Kernel(serializer='msgpack')
-kernel.register_handlers({'handle_get_remote': handle_get_remote,
+kernel.register_handlers({'handle_deref': handle_deref,
+                          'handle_del': handle_del,
+                          'handle_get_remote': handle_get_remote,
                           'handle_getattr': handle_getattr,
                           'handle_call': handle_call})
 kernel.run()
