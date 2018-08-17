21a34c7f68f40d0c9c0a468570d1c5181a1caea8
Henry Doupe <hdoupe@yahoo.com>
2018-08-17 15:02:36 -0400

    Runs with ogusa/taxcalc

---
+++ irpc/pyrpc/examples/ogusa_kernel.py
@@ -0,0 +1,15 @@
+
+def ogusa_endpoint(*args, **kwargs):
+    from ogusa.scripts import run_ogusa
+    return run_ogusa.run_micro_macro(*args, **kwargs)
+
+if __name__ == '__main__':
+    import sys
+    from rpc.kernel import Kernel
+    health_port, submit_task_port, get_task_port = sys.argv[1:]
+    print('stating kernel at ports: ', health_port, submit_task_port, get_task_port)
+    kernel = Kernel(health_port=health_port, submit_task_port=submit_task_port,
+                    get_task_port=get_task_port)
+
+    kernel.register_handlers({'ogusa_endpoint': ogusa_endpoint})
+    kernel.run()
--- irpc/pyrpc/examples/ogusa_run.py
+++ irpc/pyrpc/examples/ogusa_run.py
@@ -1,24 +1,29 @@
-import ogusa
-
 from rpc.kernelmanager import KernelManager
 from rpc.client import Client
 
-from run_ogusa_example import run_micro_macro
-
-health, rep, req = 5566, 5567, 5568
-kernel_info = [
-    {'name': 'taxcalc1',
-     'executable': '/Users/henrydoupe/anaconda3/envs/taxcalc-env/bin/python',
-     'module_path': 'taxcalc_kernel.py'},
-]
+kernel_info = {
+    'taxcalc': {
+        'executable': '/Users/henrydoupe/anaconda3/envs/taxcalc-env/bin/python',
+        'module_path': 'taxcalc_kernel.py'
+    },
+    'ogusa': {
+        'executable': '/Users/henrydoupe/anaconda3/envs/ospcdyn/bin/python',
+        'module_path': 'ogusa_kernel.py'
+     }
+}
 
 
 with KernelManager(kernel_info) as sk:
     print('running tasks...')
-    run_micro_macro(user_params={
+    user_params={
         'reform':{
             2020: {
                 '_STD': [15000, 30000, 15000, 22000, 30000]
             }
         }
-    })
+    }
+    with Client(kernel_id='ogusa') as client:
+        task = client.submit_task('ogusa_endpoint',
+                                  args=(),
+                                  kwargs={'user_params': user_params})
+        result = task.get()
