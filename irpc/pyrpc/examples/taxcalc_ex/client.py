896e88710e23b451744d6dd13e1aa6fe7626a719
Henry Doupe <hdoupe@yahoo.com>
2018-08-20 09:39:12 -0400

    Show example outside of with block

--- irpc/pyrpc/examples/taxcalc_ex/client.py
+++ irpc/pyrpc/examples/taxcalc_ex/client.py
@@ -4,16 +4,21 @@ from rpc.proxy.client import ProxyClient, deref
 
 kernel_info = {'new': {'module_path': 'kernel.py'}}
 
-with KernelManager(kernel_info) as km:
-    with ProxyClient(kernel_id='new', serializer='pickle') as client:
-        tc_proxy = client.get_remote('taxcalc')
-        rec = tc_proxy.Records.cps_constructor()
-        pol = tc_proxy.Policy()
-        reform = {2020: {'_II_em': [7000.0]}}
-        pol.implement_reform(reform)
-        calc = tc_proxy.Calculator(policy=pol, records=rec)
-        calc.advance_to_year(2020)
-        calc.calc_all()
-        m = calc.mtr('e00200p')
-        mtr = deref(m)
-        print('mtr for primary earner: ', mtr)
+km = KernelManager(kernel_info)
+km.start()
+client = ProxyClient(kernel_id='new', serializer='pickle')
+
+tc_proxy = client.get_remote('taxcalc')
+rec = tc_proxy.Records.cps_constructor()
+pol = tc_proxy.Policy()
+reform = {2020: {'_II_em': [7000.0]}}
+pol.implement_reform(reform)
+calc = tc_proxy.Calculator(policy=pol, records=rec)
+calc.advance_to_year(2020)
+calc.calc_all()
+m = calc.mtr('e00200p')
+mtr = deref(m)
+print('mtr for primary earner: ', mtr)
+
+client.close()
+km.close()
